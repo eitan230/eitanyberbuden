8b1cdee30534f1f1b3a1860ea1a277a9b40a9f70
Eitan <eitan03@hotmail.com.ar>
2023-01-02 22:15:11 -0300

    corrigiendo errores

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const form = document.querySelector(".contactoConteiner");
 const mensaje = document.querySelector(".mensajeEnviado");
 const btn = document.querySelector(".buttonSubmit")
-const input = document.documentElement.querySelector("#darkMode")
+const input = document.querySelector("html")
 
 btn.addEventListener('click', function enviarForm(evento){
     evento.preventDefault()
     form.style.display = 'none';
-    mensaje.style.display = 'flex';
-    
+    mensaje.style.display = 'flex';  
 })
 
 function dark_mode(){
